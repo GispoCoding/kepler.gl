9a2a136401a568ce446eca9fa0ce15d8d9540edb
Joonalai <joona@gispo.fi>
2020-05-04 11:42:16 +0300

    Improve array-filter
    - Save previous value to state
    - Toggle between whole day morning rush hours

--- src/components/filters/array-filter.js
+++ src/components/filters/array-filter.js
@@ -18,24 +18,25 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import ItemSelector from '../common/item-selector/item-selector';
-import {PanelLabel} from '../common/styled-components';
-import {FormattedMessage} from 'react-intl';
 import TimeRangeSliderFactory from 'components/common/time-range-slider';
 import moment from 'moment';
+import Switch from '../common/switch';
 
 ArrayFilterFactory.deps = [TimeRangeSliderFactory];
 const defaultTimeRange = [
-  moment
-    .utc()
-    .startOf('day')
-    .unix() * 1000,
   (moment
     .utc()
     .startOf('day')
     .unix() +
-    2 * 60 * 60) *
+    8 * 60 * 60) *
+    1000,
+  (moment
+    .utc()
+    .startOf('day')
+    .unix() +
+    10 * 60 * 60) *
     1000
 ];
 
@@ -65,36 +66,65 @@ export default function ArrayFilterFactory(TimeRangeSlider) {
         ),
       [filter.domain]
     );
-    const currentValue = useMemo(
-      () =>
-        filter.value && Object.prototype.toString.call(filter.value) === '[object String]'
-          ? filter.value.split(':')[0]
-          : filter.value,
-      [filter.value]
+
+    const [previousVal, setPreviousVal] = useState('Helsinki');
+
+    const isEmpty = value => value == null || value.length === 0;
+
+    const onSetFilter = useCallback(
+      value => {
+        setPreviousVal(value);
+        setFilter(idx, 'value', labeledDomainValues[value]);
+      },
+      [idx, setFilter, labeledDomainValues, setPreviousVal, previousVal]
     );
 
-    const onSetFilter = useCallback(value => setFilter(idx, 'value', labeledDomainValues[value]), [
-      idx,
-      setFilter,
-      labeledDomainValues
-    ]);
+    const currentValue = useMemo(() => {
+      let newVar =
+        Object.prototype.toString.call(filter.value) === '[object String]'
+          ? filter.value.split(':')[0]
+          : filter.value;
+      if (isEmpty(newVar)) {
+        newVar = previousVal;
+        onSetFilter(previousVal);
+      }
+      return newVar;
+    }, [filter.value, previousVal]);
+
+    const setValueAfterTimeFilter = value => {
+      const vals = filter.value.split(':');
+      const newVal =
+        vals.length === 2 ? `${filter.value}:${value}` : `${vals[0]}:${vals[1]}:${value}`;
+      setFilter(idx, 'value', newVal);
+    };
 
     const onSetTimeFilter = useCallback(
       value => {
         setFilter(idx, 'timeRange', value);
-        const vals = filter.value.split(':');
-        const newVal = vals.length === 2 ? `${filter.value}:${value}` : `${vals[0]}:${vals[1]}:${value}`;
-        setFilter(idx, 'value', newVal);
+        setValueAfterTimeFilter(value);
       },
       [idx, setFilter, filter.value]
     );
 
-    const currentTimeValue = useMemo(
-      () => (filter.timeRange ? filter.timeRange : defaultTimeRange),
-      [filter.timeRange, defaultTimeRange]
+    const currentTimeValue = useMemo(() => (filter.timeRange ? filter.timeRange : timeDomain), [
+      filter.timeRange,
+      timeDomain
+    ]);
+
+    const isTimeRangeWholeDay = useMemo(() => currentTimeValue === timeDomain, [
+      currentTimeValue,
+      timeDomain
+    ]);
+
+    const onToggleWholeDaySwitch = useCallback(
+      e => {
+        const timeRangeVal = isTimeRangeWholeDay ? defaultTimeRange : timeDomain;
+        setFilter(idx, 'timeRange', timeRangeVal);
+        setValueAfterTimeFilter(timeRangeVal);
+      },
+      [isTimeRangeWholeDay, defaultTimeRange, timeDomain, setFilter, previousVal, currentValue]
     );
 
-    //  TODO: show animation slider AND improve performance
     return (
       <div>
         <ItemSelector
@@ -104,22 +134,32 @@ export default function ArrayFilterFactory(TimeRangeSlider) {
           multiSelect={false}
           placement={filter.enlarged ? 'top' : 'bottom'}
         />
+
         {filter.type && currentValue && currentValue.length > 0 && (
-          <TimeRangeSlider
-            domain={timeDomain}
-            value={currentTimeValue}
-            plotType={filter.plotType}
-            lineChart={filter.lineChart}
-            step={1000 * 60 * 30}
-            speed={filter.speed}
-            histogram={filter.enlarged ? filter.enlargedHistogram : filter.histogram}
-            onChange={onSetTimeFilter}
-            toggleAnimation={toggleAnimation}
-            isAnimatable={isAnimatable}
-            isEnlarged={filter.enlarged}
-            timeFormat={' HH:mm'}
-            hideTimeTitle={hideTimeTitle}
-          />
+          <div>
+            <Switch
+              type="switch"
+              id="show-whole-day"
+              checked={isTimeRangeWholeDay}
+              label={'Näytä koko vuorokausi'}
+              onChange={onToggleWholeDaySwitch}
+            />
+            <TimeRangeSlider
+              domain={timeDomain}
+              value={currentTimeValue}
+              plotType={filter.plotType}
+              lineChart={filter.lineChart}
+              step={1000 * 60 * 30}
+              speed={filter.speed}
+              histogram={filter.enlarged ? filter.enlargedHistogram : filter.histogram}
+              onChange={onSetTimeFilter}
+              toggleAnimation={toggleAnimation}
+              isAnimatable={isAnimatable}
+              isEnlarged={filter.enlarged}
+              timeFormat={' HH:mm'}
+              hideTimeTitle={hideTimeTitle}
+            />
+          </div>
         )}
       </div>
     );
